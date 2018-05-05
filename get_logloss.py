30d7552e8bd601abaaea7d3cdc4e08d6c6347776
Aleksander Molak <aleksander.molak@gmail.com>
2018-05-05 12:35:00 +0200

    Update get_logloss.py

--- get_logloss.py
+++ get_logloss.py
@@ -4,14 +4,16 @@ def get_logloss(actual, pred_probs, clipper=1e-14):
     actual: an array of actual labels
     pred_probs: an array of predicted probabilities
     and one float:
-    clipper: a float defining the width of an offset from 0 and 1"""
+    clipper: a float defining the width of an offset from 0 and 1
+    Outputs:
+    logloss as a float"""
     
     if (clipper < 0) or (clipper >= 1):
         raise ValueError('`clipper` value has to be > 0 and < 1')
         
     actual = np.array(actual)
-    predicted = np.array(predicted)
+    predicted = np.array(pred_probs)
     
     predicted = np.clip(predicted, clipper, 1-clipper)
     
-    return -1 * np.mean(actual * np.log(predicted) + (1 - actual) * np.log(1 - predicted))
+    return -1 * (np.mean(actual * np.log(predicted) + (1 - actual) * np.log(1 - predicted)))
