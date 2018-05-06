e5e728b75e78719a963edcd5fbda701e58c104c8
Aleksander Molak <aleksander.molak@gmail.com>
2018-05-06 20:13:55 +0200

    Create get_vif.py

---
+++ get_vif.py
@@ -0,0 +1,17 @@
+from statsmodels.stats.outliers_influence import variance_inflation_factor as vif
+
+def get_vif(X):
+    """
+    Takes a pd.DataFrame or 2D np.array
+    and prints Variance Inflation Factor 
+    for every variable.
+    """
+    
+    if isinstance(data, pd.DataFrame) == False:
+        X = pd.DataFrame(X)
+    
+    X['__INTERCEPT'] = np.ones(X.shape[0])
+    
+    for i in range(X.shape[1]-1):
+        the_vif = vif(X.values, i)
+        print("VIF for COL{}: {}\n".format(i, the_vif))
