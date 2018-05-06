2c84d1811537abd8643f9f44649c629172264b17
Aleksander Molak <aleksander.molak@gmail.com>
2018-05-06 20:29:00 +0200

    Update get_vif.py

--- get_vif.py
+++ get_vif.py
@@ -1,11 +1,6 @@
 from statsmodels.stats.outliers_influence import variance_inflation_factor as vif
 
 def get_vif(X):
-    """
-    Takes a pd.DataFrame or 2D np.array
-    and prints Variance Inflation Factor 
-    for every variable.
-    """
     
     if isinstance(data, pd.DataFrame) == False:
         X = pd.DataFrame(X)
@@ -14,4 +9,4 @@ def get_vif(X):
     
     for i in range(X.shape[1]-1):
         the_vif = vif(X.values, i)
-        print("VIF for COL{}: {}\n".format(i, the_vif))
+        print("VIF for column {:03}: {:.02f}".format(i, the_vif))
