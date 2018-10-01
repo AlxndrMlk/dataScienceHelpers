075c67bdfa20a04ab24e08a5c311443282d59c29
Aleksander Molak <aleksander.molak@gmail.com>
2018-10-01 15:49:31 +0200

    Create end_signal.py

---
+++ end_signal.py
@@ -0,0 +1,8 @@
+def end_signal():     
+
+    # Hey, computer, let me know when you're done! :)
+    # NOTE: This only works for Windows   
+    
+    winsound.Beep(440, 100)
+    time.sleep(.05)
+    winsound.Beep(660, 100)
