193593b878191ae06845ca52a7bef0bde89215f3
Aleksander Molak <aleksander.molak@gmail.com>
2019-03-21 18:18:58 +0100

    Update img_center.py

--- img_center.py
+++ img_center.py
@@ -3,9 +3,15 @@ import numpy as np
 def image_center_crop(img):
     """
     Single image center-cropper
+    _____________________________________________________________
+    Takes a 3D image with shape (h, w, c) as np.array and 
+    returns a centered image with shape (min(h, w), min(h, w), c)
     """
     
     edge = np.min(img.shape[:2])
-    cropped_img = img[0:edge, 0:edge, :]
+
+    h, w, c = img.shape
+    
+    cropped_img = img[(h/2 - edge/2):(h/2 + edge/2), (w/2 - edge/2):(w/2 + edge/2 ), :]
     
     return cropped_img
