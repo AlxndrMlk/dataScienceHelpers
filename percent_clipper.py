a34f8118a2d76863e2e98c1d3122a567cc2b1967
Aleksander Molak <aleksander.molak@gmail.com>
2019-04-01 17:12:13 +0200

    Create percent_clipper.py

---
+++ percent_clipper.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def percent_clipper(x, percentiles):
+
+  """
+  Takes data as np.ndarray and percentiles as array-like
+  Returns clipped ndarray
+  """
+  
+  LOWERBOUND, UPPERBOUND = np.percentile(x, [percentiles[0], percentiles[1])
+  
+  return np.clip(x, LOWERBOUND, UPPERBOUND)
