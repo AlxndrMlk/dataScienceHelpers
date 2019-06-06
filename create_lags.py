56d2100f1f4f8b524c2af799498fc92e952bec07
Aleksander Molak <aleksander.molak@gmail.com>
2019-06-06 15:40:07 +0200

    Update create_lags.py

--- create_lags.py
+++ create_lags.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def create_lags(df, cols, lags):
     lagged = df.assign(**{
-            '{} (t-{})'.format(col, t): bb[col].shift(t)
+            '{} (t-{})'.format(col, t): df[col].shift(t)
                     for t in lags
                     for col in cols
             })
